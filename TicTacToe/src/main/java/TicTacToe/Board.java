400310910f06dd626688e5be355a90d2a6797350
Deiwo <dawidniesterczuk@gmail.com>
2022-11-01 11:41:41 +0100

    commit

---
+++ TicTacToe/src/main/java/TicTacToe/Board.java
@@ -0,0 +1,31 @@
+package TicTacToe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Board {
+    public List<Character> board = new ArrayList<>();
+    public int boardSize;
+
+    public Board(){}
+
+    public void preparingBoard(int size){
+        for(int i=0; i < size * size; i++){
+            board.add('-');
+        }
+    }
+
+    public void resetBoard(){
+        for(int i=0; i <getBoardSize()*getBoardSize(); i++){
+            board.set(i, '-');
+        }
+    }
+    public int getBoardSize() {
+        return boardSize;
+    }
+    public void setBoardSize(int boardSize) {
+        this.boardSize = boardSize;
+    }
+
+}
+
